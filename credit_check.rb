2779814783352782eda39e8cf143135c3a1f0c8d
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-06-26 15:24:43 -0600

    adds new file

---
+++ credit_check.rb
@@ -0,0 +1,15 @@
+puts "hello"
+
+card_number = "4929735477250543"
+
+# 2 * every other digit: 8 9 4 9 14 3 10 4 14 7 4 5 0 5 8 3
+# Summed digit > 10:     8 9 4 9  5 3  1 4  5 7 4 5 0 5 8 3
+# Results summed:        = 80
+# Modulo Ten:            80 % 10 == 0
+
+Modulo Ten == 0
+
+if Modulo Ten == 0
+  puts "The number is valid"
+else
+  puts "The number is invalid"
